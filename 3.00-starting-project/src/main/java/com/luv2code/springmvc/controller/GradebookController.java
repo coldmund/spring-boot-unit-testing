9c59b8fdd999fb34c1120d8be0341b4fc7ae5367
Joon-yong Hong <joonyong.hong@lge.com>
2025-03-31 16:55:06 +0900

    section 8

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springmvc.controller;
 
 import com.luv2code.springmvc.models.*;
+import com.luv2code.springmvc.service.StudentAndGradeService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,11 +14,37 @@ public class GradebookController {
 	@Autowired
 	private Gradebook gradebook;
 
+	@Autowired
+	private StudentAndGradeService studentService;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getStudents(Model m) {
+		Iterable<CollegeStudent> collegeStudents = studentService.getGradebook();
+		m.addAttribute("students", collegeStudents);
 		return "index";
 	}
 
+	@PostMapping(value = "/")
+	public String createStudent(@ModelAttribute("student") CollegeStudent student, Model m) {
+
+		studentService.createStudent(student.getFirstname(), student.getLastname(), student.getEmailAddress());
+		Iterable<CollegeStudent> collegeStudents = studentService.getGradebook();
+		m.addAttribute("students", collegeStudents);
+		return "index";
+	}
+
+	@GetMapping("/delete/student/{id}")
+	public String deleteStudent(@PathVariable int id, Model m) {
+
+		if (!studentService.checkIfStudentIsNull(id)) {
+			return "error";
+		}
+
+		studentService.deleteStudentService(id);
+		Iterable<CollegeStudent> collegeStudents = studentService.getGradebook();
+		m.addAttribute("student", collegeStudents);
+		return "index";
+	}
 
 	@GetMapping("/studentInformation/{id}")
 		public String studentInformation(@PathVariable int id, Model m) {
